04f4e4bae738e7de63f69dd93bf4b2804f9a8a48
이세은 <thjds8@gmail.com>
2017-05-29 17:46:58 +0900

    5.29 prof

--- TravelWM/src/main/java/com/pitapany/web/controller/RootController.java
+++ TravelWM/src/main/java/com/pitapany/web/controller/RootController.java
@@ -17,8 +17,8 @@ public class RootController {
 		String role = "ROLE_USER";
 		
 		if(role.equals("ROLE_USER"))
-			return "member.index";
+			return "profile.home";
 		else
-			return "member.index";
+			return "profile.home";
 	}
 }
