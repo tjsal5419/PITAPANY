8388b9759e6018a6401564f70a43f3d0007f829d
SeonMi <SeonMi@Ego>
2017-06-13 21:39:36 +0900

    팁게시판

--- TravelWM/src/main/java/com/pitapany/web/controller/CommunityController.java
+++ TravelWM/src/main/java/com/pitapany/web/controller/CommunityController.java
@@ -36,12 +36,56 @@ public class CommunityController {
 	@RequestMapping("tip")
 	public String tip(HttpServletRequest request,
 	Model model,
-	@RequestParam(value="p", defaultValue="1")int page) {
+	@RequestParam(value="p", defaultValue="1")int page,
+	@RequestParam(value="f", defaultValue="title")String field,
+	@RequestParam(value="q", defaultValue="")String query,
+	@RequestParam(value="c", defaultValue="%%")String category) {
+		System.out.println(field);
+
 		List<TipCategory> categoryList = tipCategoryDao.getList();
 		model.addAttribute("categoryList", categoryList);
+		
 
-		List<CommunityTipView> list = communitytipDao.getList();
+		List<CommunityTipView>	list = communitytipDao.getList(page,field, query, category);
 		model.addAttribute("tipList", list);
+
+		model.addAttribute("category",category);
+		model.addAttribute("page", page);
+		int count = communitytipDao.count(field, query,category);
+		int pageCount=0;
+		
+		if(count%10==0)
+		{
+			pageCount = count/10;
+		}
+		else{
+			pageCount = count/10+1;
+		}
+		
+		model.addAttribute("pageCount",pageCount);
+		
+		int prev = (page/10)*10;
+		int next = 0;
+		
+		if(page<10)
+			prev = 1;
+	
+		int pageBarCount = pageCount/10;
+		
+		if(page/10!=pageBarCount){
+			if(prev/10==0)
+				next = 9;
+			else 
+				next = prev+9;
+		}
+		else
+			next = pageCount;
+
+		
+		/*System.out.println("page:"+page+"prev"+prev+"next:"+next);*/
+		model.addAttribute("prev",prev);
+		model.addAttribute("next",next);
+		
 		return "community.tip";
 	}
 
@@ -87,13 +131,44 @@ public class CommunityController {
 	@RequestMapping(value="tip-detail",
 			method=RequestMethod.GET)
 	public String tipDetail(Model model,String id) {
+		communitytipDao.addHits(id);
 
 		CommunityTipView communityTipView = communitytipDao.get(id);
+		
 		model.addAttribute("tipView",communityTipView);
 		
 		return "community.tip-detail";
 	}
+	
+	@RequestMapping(value="tip-delete",
+			method=RequestMethod.GET)
+	public String tipDelete(String id){
+		int result = communitytipDao.delete(id);
+		if(result==1)
+			return "redirect:tip";
+		return "";
+	}
 
+	@RequestMapping(value="tip-update",
+			method=RequestMethod.GET)
+	public String tipUpdate(Model model,String id){
+		CommunityTip tipView = communitytipDao.get(id);
+		
+		List<TipCategory> categoryList = tipCategoryDao.getList();
+		System.out.println(tipView.getId());
+		model.addAttribute("tipView",tipView);
+		model.addAttribute("categoryList", categoryList);
+		return "community.tip-update";
+	}
+
+	@RequestMapping(value="tip-update",
+			method=RequestMethod.POST)
+	public String tipUpdate(CommunityTip tipView){
+		if(communitytipDao.update(tipView)==1)
+			return "redirect:tip?id="+tipView.getId();
+		return "";
+	}
+	
 	@RequestMapping("faq")
 	public String faq() {
 		return "community.faq";
