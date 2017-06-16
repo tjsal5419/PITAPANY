4993ba6910d018aa51b371900df2810ecf05c99e
wonjun <wonjun@192.168.0.103>
2017-06-16 12:10:01 +0900

    acc-detail back(working)

--- TravelWM/src/main/java/com/pitapany/web/controller/AccompanyController.java
+++ TravelWM/src/main/java/com/pitapany/web/controller/AccompanyController.java
@@ -6,38 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.pitapany.web.dao.AccompanyBoardDao;
 import com.pitapany.web.dao.StyleDao;
+import com.pitapany.web.entity.AccompanyBoard;
 import com.pitapany.web.entity.Style;
 
 @Controller
 @RequestMapping("/accompany/*")
 public class AccompanyController {
-	
+
+	@Autowired
+	private AccompanyBoardDao accompanyBoardDao;
+
 	@Autowired
 	private StyleDao styleDao;
-	
+
 	@RequestMapping("/matching")
-	public String  matching(Model model){
+	public String matching(Model model) {
 		List<Style> list = styleDao.getList();
 		model.addAttribute("styles", list);
 		return "accompany.matching";
 	}
-	
 
-	@RequestMapping("/detail")
-	public String  detail(Model model){
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
+	public String detail(Model model, String id) {
+		accompanyBoardDao.addHits(id);
+
+		AccompanyBoard accompanyBoard = accompanyBoardDao.get(id);
+		
+		model.addAttribute("accDetail", accompanyBoard);
+
 		return "accompany.detail";
 	}
-	
+
 	@RequestMapping("/reg")
-	public String  reg(Model model){
+	public String reg(Model model) {
 		return "accompany.reg";
 
 	}
-	
+
 	@RequestMapping("/board")
-	public String  board(Model model){
+	public String board(Model model) {
 		return "accompany.board";
 
 	}
